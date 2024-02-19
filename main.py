33b5126107ac29a6148a2870c0b7533523526a05
dudelim <137728498+dudelim@users.noreply.github.com>
2024-02-19 16:25:50 +0000

    Update

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ testes = ["Miritiba 339", "Babaçu 500", "Cambuí 804B"]
 for teste in testes:
     rua, numero = str_endereco(teste)
     print(f"'{teste}' = '{rua}', '{numero}'")
+
+
+# Caso complicado
+def str_endereco(endereco):
+    parte_endereco = endereco.split()  # Divide o endereço
+    nome_rua = ''
+    numero_rua = ''
+
+    for i, parte in enumerate(parte_endereco):
+          if parte_endereco.isdigit() or (parte_endereco[0].isdigit() and i > 0 and not parte_endereco[i-1].isdigit()):  # Espaço núm/letra
+            
