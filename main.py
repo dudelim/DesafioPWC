1dfa865b5159972adfa995a10a556068209b717f
dudelim <137728498+dudelim@users.noreply.github.com>
2024-02-19 16:21:17 +0000

    Update

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # Caso simples:
 def str_endereco(endereco):
-  parte_endereco = endereco.split() # Divide o endereço
-  nome_rua = ''
-  numero_rua = ''
+    parte_endereco = endereco.split()  # Divide o endereço
+    nome_rua = ''
+    numero_rua = ''
 
-  for i in parte_endereco:
-      if i.isdigit():  # Se o i for um número 
-          numero_rua = parte_endereco
-      else:  # Se o i não for um número 
-          nome_rua += parte_endereco + ' '  
-      return nome_rua.strip(), numero_rua
-  
+    for i in parte_endereco:
+        if i.isdigit():  # Se o i for um número 
+            numero_rua = i
+        else:  # Se o i não for um número 
+            nome_rua += i + ' '
+
+    return nome_rua.strip(), numero_rua
+
+testes = ["Miritiba 339", "Babaçu 500", "Cambuí 804B"]
+
+for teste in testes:
+    rua, numero = str_endereco(teste)
+    print(f"'{teste}' = '{rua}', '{numero}'")
